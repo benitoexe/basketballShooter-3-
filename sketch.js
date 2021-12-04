6125633b3063cf54a886a58df50582081819b69f
bi9017869 <73246581+bi9017869@users.noreply.github.com>
2021-12-04 09:52:57 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var restartImg;
 var restartButton;
 var hits=0;
 var score=0;
+var gamestate="play";
 
 function preload(){
 hoopImg=loadImage("hoop.png")
@@ -19,13 +20,16 @@ backgroundSound = loadSound("basketball_beat.mp3");
 function setup(){
 
   createCanvas(600,700);
+ 
+  if(gamestate=="play")
+{
   backgroundSound.loop();
 
+  restartButton=createButton("Restart");
+  restartButton.position(420,600);
+  //restartButton.size(50,50);
+   restartButton.mouseClicked(hitAgain)
 
-restartButton=createButton("Restart");
-restartButton.position(420,600);
-//restartButton.size(50,50);
-restartButton.mouseClicked(hitAgain)
 
  hoop=createSprite(300,10)
  hoop.addImage(hoopImg);
@@ -38,7 +42,7 @@ hoop.setCollider("circle",0,0,100);
  basketball.scale = 0.2;
  basketball.debug=false;
 
- obstacle=createSprite(Math.round(random(50,500)),400,50,50);
+ obstacle=createSprite(Math.round(random(50,500)),100,50,50);
  obstacle.addImage(obstacleImg);
  obstacle.scale = 0.2;
  obstacle.velocityX = 3;
@@ -47,9 +51,10 @@ hoop.setCollider("circle",0,0,100);
  hoop.velocityX=5;
  edges=createEdgeSprites();
 }
+}
 
 function hitAgain(){
-  console.log("hit")
+ 
 basketball.x=300;
 basketball.y=400;
 basketball.velocityY = 0;
@@ -57,12 +62,13 @@ basketball.velocityY = 0;
 
 function draw(){
   background("green");
-
+if(gamestate=="play"){
+  
   textSize(20);
   fill("black")
   text("Score:"+score,500,50);
   //moving  the basketball with the arrow keys
-  if(keyDown("UP_ARROW")){
+  if(keyWentDown("UP_ARROW")){
     basketball.velocityY = -19;
     hits = hits+1;
   }
@@ -98,26 +104,39 @@ else{
 
  }
  
- if(obstacle.isTouching(basketball)){
-   basketball.velocityY = 0;
-   obstacle.velocityY = 0;
-   obstacle.velocityX = 0;
- }
- if(hits>=3){
-   text("GAME OVER: YOU LOST!",300,300);
-   hitAgain();
-   obstacle.velocityX = 0;
-   obstacle.velocityY = 0;
- }
-  //spawnObstacle();
-  drawSprites();
+ if(obstacle.isTouching(basketball) ||hits>3){
+      gamestate="end"
+   }
+   drawSprites();
+}
+else if(gamestate=="end"){
+  background("black");
+  textSize(30)
+  fill("yellow")
+  text("GAME OVER: YOU LOST!",200,300);
+  basketball.visible=false;
+  obstacle.visible=false;
+//  hoop.visible=false;
+  restartButton.mouseClicked(reset)
+  
+}
+ 
 
 }
-function spawnObstacle(){
-  if(frameCount%60==0){
-    var obstacle=createSprite(Math.round(random(50,500)),400,50,50);
-    obstacle.scale=0.2;
-    obstacle.addImage(obstacleImg);
-    
-  }
+
+function reset(){
+  gamestate="play";
+  basketball.visible=true;
+  obstacle.visible=true;
+  hoop.visible=true;
+  console.log("restart");
+  score=0;
+  hits=0;
+  basketball.x=300;
+  basketball.y=400;
+  basketball.velocityY=0;
+  obstacle.velocityX = 3;
+ obstacle.velocityY = 4;
+
+ hoop.velocityX=5;
 }
